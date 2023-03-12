a45192ef29d0b9dbf12235ada796853ee44cf847
ganesh4946 <122338021+ganesh4946@users.noreply.github.com>
2023-03-12 11:54:16 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function Resume() {
+  const [personalInfo, setPersonalInfo] = useState({
+    name: 'John Doe',
+    email: 'johndoe@email.com',
+    phone: '555-555-5555',
+    address: '123 Main St, Anytown, USA'
+  });
+
+  const [workExperience, setWorkExperience] = useState([
+    {
+      position: 'Software Developer',
+      company: 'ABC Company',
+      startDate: 'January 2018',
+      endDate: 'Present',
+      description: 'Developed and maintained software applications'
+    },
+    {
+      position: 'Web Developer',
+      company: 'XYZ Company',
+      startDate: 'June 2015',
+      endDate: 'December 2017',
+      description: 'Built and maintained websites'
+    }
+  ]);
+
+  const [education, setEducation] = useState([
+    {
+      degree: 'Bachelor of Science in Computer Science',
+      school: 'University of Anytown',
+      graduationDate: 'May 2015'
+    }
+  ]);
+
+  const [skills, setSkills] = useState([
+    'JavaScript',
+    'React',
+    'HTML',
+    'CSS',
+    'Node.js',
+    'Git'
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{personalInfo.name}</h1>
+      <p>{personalInfo.email}</p>
+      <p>{personalInfo.phone}</p>
+      <p>{personalInfo.address}</p>
+
+      <h2>Work Experience</h2>
+      {workExperience.map((job, index) => (
+        <div key={index}>
+          <h3>{job.position}</h3>
+          <p>{job.company}</p>
+          <p>{job.startDate} - {job.endDate}</p>
+          <p>{job.description}</p>
+        </div>
+      ))}
+
+      <h2>Education</h2>
+      {education.map((degree, index) => (
+        <div key={index}>
+          <h3>{degree.degree}</h3>
+          <p>{degree.school}</p>
+          <p>{degree.graduationDate}</p>
+        </div>
+      ))}
+
+      <h2>Skills</h2>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default Resume;
